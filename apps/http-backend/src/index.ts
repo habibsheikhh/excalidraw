74e19ae69872d72eb90eb218b6e2f61639262865
habibsheikhh <habibsheikh4748@gmail.com>
2025-07-22 01:45:44 +0530

    Fixed Canvas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,7 +8,9 @@ import bcrypt from "bcrypt"
 import cors from "cors"
 const app = express(); 
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 app.use(express.json())
 
